d95097d330f4617e35946554ad884f681eb664d9
Richard Freire <richardfreire@MacBook-Air-M1-de-Richard.local>
2022-08-13 17:38:48 -0300

    Terminando teste

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,37 +1,39 @@
-import React from 'react';
-import { useTheme } from 'styled-components';
-import { Fontisto } from '@expo/vector-icons'
-import { ActivityIndicator, Alert, Modal, View } from 'react-native';
+import { Fontisto } from "@expo/vector-icons";
+import { ActivityIndicator, Alert, Modal, View } from "react-native";
+import { useTheme } from "styled-components";
 
-import { useAuth } from '../../hooks/useAuth';
+import { useAuth } from "../../hooks/useAuth";
 
-import LoginBannerImg from '../../assets/images/login.svg';
-import LogoImg from '../../assets/images/logo.svg';
+import LoginBannerImg from "../../assets/images/login.svg";
+import LogoImg from "../../assets/images/logo.svg";
 
-import { 
+import {
   Container,
   Content,
-  LoginBanner, 
-  LoginInfo, 
-  Header, 
-  Partner, 
-  Description, 
+  Description,
+  Header,
+  LoginBanner,
+  LoginInfo,
+  Partner,
   SignInButton,
   SignInButtonIcon,
-  SignInButtonText 
-} from './styles';
+  SignInButtonText,
+} from "./styles";
 
 export function SignIn() {
   const { signIn, isLoggingIn } = useAuth();
   const theme = useTheme();
 
   // creates a function to handle sign in
-    // try to call and wait signIn
-    // if fails, display an Alert with the title "Erro SignIn" and message "Ocorreu um erro ao tentar logar no app"
-
-    // const signInButtonProps = {
-    //   onPress: your-signIn-function
-    // }
+  // try to call and wait signIn
+  // if fails, display an Alert with the title "Erro SignIn" and message "Ocorreu um erro ao tentar logar no app"
+  async function handleSignIn() {
+    try {
+      await signIn();
+    } catch (error) {
+      Alert.alert("Erro SignIn", "Ocorreu um erro ao tentar logar no app");
+    }
+  }
 
   return (
     <Container
@@ -60,36 +62,44 @@ export function SignIn() {
           </Header>
 
           <Description>
-            Veja dados{'\n'}
-            interessantes sobre{'\n'}
-            o mundo da Twitch
+            Veja dados{"\n"}
+            interessantes sobre{"\n"}o mundo da Twitch
           </Description>
 
-          {/* <SignInButton {...signInButtonProps}>
+          {/* <SignInButton onPress={}>
             <SignInButtonIcon>
               Verify if isLoggingIn is true
               If it is, show an ActivityIndicator
               Otherwise, show Fontisto's twitch icon
             </SignInButtonIcon>
-
             <SignInButtonText>
               Verify if isLoggingIn is true
               If it is, show "Entrando..."
               Otherwise, show "Entrar com Twitch"
             </SignInButtonText>
           </SignInButton> */}
+          <SignInButton onPress={handleSignIn}>
+            <SignInButtonIcon>
+              {isLoggingIn ? (
+                <ActivityIndicator color={theme.colors.white} />
+              ) : (
+                <Fontisto name="twitch" color={theme.colors.white} size={22} />
+              )}
+            </SignInButtonIcon>
+            <SignInButtonText>
+              {isLoggingIn ? "Entrando ..." : "Entrar com Twich"}
+            </SignInButtonText>
+          </SignInButton>
         </LoginInfo>
       </Content>
 
-      <Modal 
+      <Modal
         animationType="fade"
         visible={isLoggingIn}
         statusBarTranslucent
         transparent
       >
-        <View
-          style={{ flex: 1, backgroundColor: 'rgba(14, 14, 16, 0.5)' }}
-        />
+        <View style={{ flex: 1, backgroundColor: "rgba(14, 14, 16, 0.5)" }} />
       </Modal>
     </Container>
   );
